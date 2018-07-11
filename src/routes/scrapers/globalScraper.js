bd24b0b61e754ab85db1583f8610d8354e2e8c4f
mGolestan <m.golestan98@gmail.com>
2018-07-11 16:18:30 +0430

    closes #5

---
+++ src/routes/scrapers/globalScraper.js
@@ -0,0 +1,35 @@
+// @flow
+import { Router as router } from "express";
+import { wrap } from "async-middleware";
+import { BadRequest } from "../../utils/errors";
+import { Place } from "../../utils/city";
+
+import "../../utils/city/data";
+
+const route = router();
+
+route.get(
+  "/",
+  wrap((req: express$Request, res: express$Response) => {
+    const { city, q } = req.query;
+
+    if (!q) {
+      throw new BadRequest("q parameter is required");
+    }
+    if (!Place.findByCitySlug(city)) {
+      throw new BadRequest("entered city is not supported");
+    }
+
+    // $flow-disable-line
+    const urlQueries = `?city=${city}&q=${q}`;
+
+    const scrapersUrl = {
+      Divar: `http://${req.headers.host}/scrape/divar${urlQueries}`,
+      Sheypoor: `http://${req.headers.host}/scrape/sheypoor${urlQueries}`
+    };
+
+    res.send(scrapersUrl);
+  })
+);
+
+export default route;
--- src/routes/scrapers/index.js
+++ src/routes/scrapers/index.js
@@ -2,10 +2,12 @@
 import { Router as router } from "express";
 import sheypoor from "./sheypoor";
 import divar from "./divar";
+import globalScraper from "./globalScraper";
 
 const route = router();
 
 route.use("/sheypoor", sheypoor);
 route.use("/divar", divar);
+route.use("/", globalScraper);
 
 export default route;
