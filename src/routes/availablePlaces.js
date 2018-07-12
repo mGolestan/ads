98a498a02e673f3c914f0e31249a9b69e6b3b173
mGolestan <m.golestan98@gmail.com>
2018-07-12 11:20:22 +0430

    feat(Place): changed the places import solution, closes #11

--- src/routes/availablePlaces.js
+++ src/routes/availablePlaces.js
@@ -1,16 +1,14 @@
 // @flow
 import { Router as router } from "express";
 import { wrap } from "async-middleware";
-import { Place } from "../utils/city";
-
-import "../utils/city/data";
+import { Place } from "../utils/places";
 
 const route = router();
 
 route.get(
   "/",
   wrap((req: express$Request, res: express$Response) =>
-    res.send(Place.getAll())
+    res.send({ results: Place.getAll() })
   )
 );
 
